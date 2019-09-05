8259d215eca67843802d424d8e0d76eedfb5eab3
Awesomeosity <lekev001@gmail.com>
2019-09-05 17:21:29 -0600

    Moved js to js file. Added background.

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+
+function load()
+{
+	let config = 
+	{
+		type: Phaser.AUTO,
+		width: 800,
+		height: 600,
+		scene:
+		{
+			preload: preload,
+			create: create,
+			update: update
+		}
+	};
+	
+	let game = new Phaser.Game(config);
+	
+}
+
+function preload()
+{
+	this.load.image('background', '../images/background.jpg');
+}
+
+function create()
+{
+	this.add.image(400, 300, 'background');
+}
+
+function update()
+{
+	
+}
