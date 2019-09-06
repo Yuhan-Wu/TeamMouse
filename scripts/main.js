5a3efd107e474a934515d077fe424355a74e9b90
Awesomeosity <lekev001@gmail.com>
2019-09-05 18:01:29 -0600

    More tutorial progress.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 let platforms;
+let player;
+let cursors;
 
 function load()
 {
@@ -24,25 +26,77 @@ function load()
 	};
 	
 	let game = new Phaser.Game(config);
-	
 }
 
 function preload()
 {
 	this.load.image('background', '../images/background.jpg');
 	this.load.image('ground', '../images/platform.png');
+	this.load.spritesheet('dude', '../images/dude.png', { frameWidth: 32, frameHeight: 48 });
+
 }
 
 function create()
-{
+{	
 	this.add.image(400, 300, 'background');
 	
 	platforms = this.physics.add.staticGroup();
 	
 	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+	
+	player = this.physics.add.sprite(100, 450, 'dude');
+	player.setBounce(0.2);
+	player.setCollideWorldBounds(true);
+	
+	cursors = this.input.keyboard.createCursorKeys();
+
+	
+	this.anims.create({
+		key: 'left',
+		frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+		frameRate:10,
+		repeat: -1
+	});
+	
+	this.anims.create({
+		key: 'turn',
+		frames: [ {key: 'dude', frame: 4}],
+		frameRate: 20
+	});
+	
+	this.anims.create({
+		key: 'right',
+		frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+		frameRate: 10,
+		repeat: -1
+	});
+	
+	this.physics.add.collider(player, platforms);
 }
 
 function update()
 {
-	
+	if (cursors.left.isDown)
+	{
+		player.setVelocityX(-160);
+
+		player.anims.play('left', true);
+	}
+	else if (cursors.right.isDown)
+	{
+		player.setVelocityX(160);
+
+		player.anims.play('right', true);
+	}
+	else
+	{
+		player.setVelocityX(0);
+
+		player.anims.play('turn');
+	}
+
+	if (cursors.up.isDown && player.body.touching.down)
+	{
+		player.setVelocityY(-330);
+	}
 }
