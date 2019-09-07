358ad2f23bd65ba7c9290356007a5d959c3f23e1
Yuhan-Wu <54376003+yuhan-wu@users.noreply.github.com>
2019-09-07 17:05:48 -0600

    experiment

--- scripts/Mouse.js
+++ scripts/Mouse.js
@@ -15,56 +15,60 @@ class Mouse extends Phaser.Physics.Arcade.Sprite {
 
     update(cursors) {
 		this.cursors = cursors;
+		this.normalMovement();
         if(this.isOnLadder)
         {
-			if(this.isClimbing)
-			{
-				this.body.velocity.x = 0;
-				this.body.allowGravity = false;
-				this.climbingMovement();
+        	this.climbingMovement();
+        	if(this.isClimbing){
+				this.x=400;
+        		this.body.velocity.x=0;
 			}
-			else
-			{
-				if(this.body.velocity.y == 0)
-				{
-					if(this.cursors.up.isDown)
-					{
-						this.body.velocity.y = -40;
-						this.isClimbing=true;
-						this.body.allowGravity = false;
-
-					}
-					else if(this.cursors.down.isDown)
-					{
-						this.body.velocity.y = 40;
-						this.isClimbing=true;
-						this.body.allowGravity = false;
-
-					}
-					else
-					{
-						this.normalMovement();
-					}
-				}
-				else
-				{
-					this.normalMovement();
-				}
-			}
+		// 	if(this.isClimbing)
+		// 	{
+		// 		//TODO x of ladder
+		// 		this.x=400;
+		// 		this.anims.play('climb');
+		// 		this.body.velocity.x = 0;
+		// 		this.body.allowGravity = false;
+		// 		this.climbingMovement();
+		// 	}
+		// 	else
+		// 	{
+		// 		if(this.body.velocity.y == 0)
+		// 		{
+		// 			if(this.cursors.up.isDown)
+		// 			{
+		//
+		// 				this.body.velocity.y = -40;
+		// 				this.isClimbing=true;
+		// 				this.body.allowGravity = false;
+		// 			}
+		// 			else if(this.cursors.down.isDown)
+		// 			{
+		// 				this.body.velocity.y = 40;
+		// 				this.isClimbing=true;
+		// 				this.body.allowGravity = false;
+		//
+		// 			}
+		// 			else
+		// 			{
+		// 				this.normalMovement();
+		// 			}
+		// 		}
+		// 		else
+		// 		{
+		// 			this.normalMovement();
+		// 		}
+		// 	}
         }
-		else
-		{
-			this.normalMovement();
-		}
+		// else
+		// {
+		// 	this.normalMovement();
+		// }
     }
 	
 	normalMovement()
 	{
-		if(this.cursors.up.isDown && this.body.touching.down && this.body.velocity.y == 0)
-		{
-			this.body.velocity.y = -200;
-		}
-
 		if (this.cursors.left.isDown)
 		{
 			this.lastDir = true;
@@ -112,16 +116,16 @@ class Mouse extends Phaser.Physics.Arcade.Sprite {
 		{
 			this.body.velocity.y = 0;
 		}
-		
-		if (this.lastDir == null || this.lastDir === false)
-		{
-			this.anims.play('leftStop');
-
-		}
-		else
-		{
-			this.anims.play('rightStop');
-		}
+		this.anims.play('stand');
+		// if (this.lastDir == null || this.lastDir === false)
+		// {
+		// 	this.anims.play('leftStop');
+		//
+		// }
+		// else
+		// {
+		// 	this.anims.play('rightStop');
+		// }
 
 	}
 
--- scripts/example_scene.js
+++ scripts/example_scene.js
@@ -14,15 +14,17 @@ class ExampleScene extends Phaser.Scene{
         this.add.image(400, 300, 'background');
 
         this.ladders = this.physics.add.group();
-        this.ladders.create(400, 400, 'ladder');
+        this.ladders.create(400, 509, 'ladder');
         Phaser.Actions.Call(this.ladders.getChildren(), function (ladder) {
             ladder.body.allowGravity = false;
         },this);
 
-        this.platforms = this.physics.add.staticGroup();
-        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-		this.platforms.create(400, 368, 'ground');
+        this.grounds=this.physics.add.staticGroup();
+        this.grounds.create(400, 568, 'ground').setScale(2).refreshBody();
 
+        this.platforms = this.physics.add.staticGroup();
+		this.platforms.create(625, 435, 'ground');
+		this.platforms.create(175,435,'ground');
 
         this.mouse=new Mouse({
             scene:this,
@@ -34,45 +36,24 @@ class ExampleScene extends Phaser.Scene{
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        // this.anims.create({
-        //     key: 'left',
-        //     frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
-        //     frameRate:10,
-        //     repeat: -1
-        // });
-        //
-        // this.anims.create({
-        //     key: 'leftStop',
-        //     frames: [ {key: 'dude', frame: 5}],
-        //     frameRate: 20
-        // });
-        // this.anims.create({
-        //     key: 'rightStop',
-        //     frames: [ {key: 'dude', frame: 0}],
-        //     frameRate: 20
-        // });
-        // this.anims.create({
-        //     key: 'right',
-        //     frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
 		let that = this;
 
-		this.physics.add.collider(this.mouse,this.platforms, (d) =>{
-			if(that.mouse.isClimbing)
-			{				
-				if(that.mouse.body.touching.up)
-				{
-					that.mouse.body.position.y -= 75;
-				}
-				else
-				{
-					that.mouse.isClimbing = false;
-					that.mouse.body.allowGravity = true;
-				}
-			}
-		});
+		this.physics.add.collider(this.mouse,this.grounds);
+		// this.physics.add.collider(this.mouse,this.platforms, () =>{
+		// 	if(that.mouse.isClimbing)
+		// 	{
+			    // that.mouse.body.x=400;
+				// if(that.mouse.body.touching.up)
+				// {
+				// 	that.mouse.body.position.y -= 75;
+				// }
+				// else
+				// {
+				// 	that.mouse.isClimbing = false;
+				// 	that.mouse.body.allowGravity = true;
+				// }
+			// }
+		// });
 
     }
 
@@ -84,54 +65,6 @@ class ExampleScene extends Phaser.Scene{
 			that.mouse.isOnLadder = true;
 		});
         this.mouse.update(this.cursors);
-    //     this.isOnLadder = false;
-    //     this.physics.overlap(this.player,this.ladders,this.ladderCheck,null,this);
-    //
-    //     if(this.isOnLadder)
-    //     {
-    //         if(this.cursors.up.isDown)
-    //         {
-    //             this.player.setVelocityY(-40);
-    //             this.isClimbing=true;
-    //         }
-    //         else if(this.cursors.down.isDown)
-    //         {
-    //             this.player.setVelocityY(40);
-    //             this.isClimbing=true;
-    //         }
-    //         else if(!this.cursors.down.isDown && !this.cursors.up.isDown)
-    //         {
-    //             this.player.setGravity(0);
-    //             this.player.setVelocityY(-5);
-    //         }
-    //
-    //     }
-    //     else if(this.cursors.up.isDown && this.player.body.touching.down)
-    //     {
-    //         this.player.setVelocityY(-200);
-    //     }
-    //
-    //     if(!this.isClimbing) {
-    //         if (this.cursors.left.isDown) {
-    //             this.lastDir = true;
-    //             this.player.setVelocityX(-80);
-    //
-    //             this.player.anims.play('left', true);
-    //         } else if (this.cursors.right.isDown) {
-    //             this.lastDir = false;
-    //             this.player.setVelocityX(80);
-    //
-    //             this.player.anims.play('right', true);
-    //         } else {
-    //             this.player.setVelocityX(0);
-    //             if (this.lastDir == null || this.lastDir === false) {
-    //                 this.player.anims.play('leftStop');
-    //
-    //             } else {
-    //                 this.player.anims.play('rightStop');
-    //             }
-    //         }
-    //     }
     }
 	
 }
